057c18a328782f817d50309cc180ffc70f079de4
JSHEN9085 <jshen9085@gmail.com>
2020-09-21 22:10:02 -0400

    added votes

--- spring-reddit/src/main/java/com/jshen9085/reddit/mapper/PostMapper.java
+++ spring-reddit/src/main/java/com/jshen9085/reddit/mapper/PostMapper.java
@@ -1,26 +1,49 @@
 package com.jshen9085.reddit.mapper;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.jshen9085.reddit.dto.PostRequest;
 import com.jshen9085.reddit.dto.PostResponse;
 import com.jshen9085.reddit.model.Post;
 import com.jshen9085.reddit.model.Subreddit;
 import com.jshen9085.reddit.model.User;
+import com.jshen9085.reddit.repository.CommentRepository;
+import com.jshen9085.reddit.repository.VoteRepository;
+import com.jshen9085.reddit.service.AuthService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private AuthService authService;
+
 
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "subreddit", source = "subreddit")
+    @Mapping(target = "voteCount", constant = "0")
     @Mapping(target = "user", source = "user")
-    Post map(PostRequest postRequest, Subreddit subreddit, User user);
+    public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
 
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 
 }
